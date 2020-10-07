b2d2aea62d3cfd1a8e224d4e6b5d12fad16eea96
Daniel Thornburgh <mysterymath@gmail.com>
2020-10-07 11:49:58 -0700

    Add minimum compiling LLVM target for MOS6502.

---
+++ llvm/lib/Target/MOS6502/MCTargetDesc/MOS6502MCTargetDesc.cpp
@@ -0,0 +1,3 @@
+#include "llvm/Support/TargetRegistry.h"
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMOS6502TargetMC() {}
---
+++ llvm/lib/Target/MOS6502/MOS6502TargetMachine.cpp
@@ -0,0 +1,4 @@
+#include "llvm/Support/TargetRegistry.h"
+
+using namespace llvm;
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeMOS6502Target() {}
