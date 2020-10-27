aeb68027e2c64feb27832a6bfbe8dedec4a397e8
Daniel Thornburgh <mysterymath@gmail.com>
2020-10-26 23:49:24 -0700

    Expand out RETURN to 8 bits.

--- llvm/lib/Target/MOS6502/MOS6502CallLowering.cpp
+++ llvm/lib/Target/MOS6502/MOS6502CallLowering.cpp
@@ -1,7 +1,10 @@
 #include "MOS6502CallLowering.h"
 #include "MCTargetDesc/MOS6502MCTargetDesc.h"
+
+#include "llvm/CodeGen/Analysis.h"
 #include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
+#include <memory>
 
 using namespace llvm;
 
@@ -10,10 +13,39 @@ bool MOS6502CallLowering::lowerReturn(MachineIRBuilder &MIRBuilder,
                                       ArrayRef<Register> VRegs) const {
   if (!Val) return true;
 
-  MachineInstrBuilder Instr = MIRBuilder.buildInstr(MOS6502::RETURN);
-  for (const Register& VReg : VRegs) {
-    Instr.addUse(VReg);
+  MachineFunction &MF = MIRBuilder.getMF();
+  const Function &F = MF.getFunction();
+  const auto &DL = F.getParent()->getDataLayout();
+
+  SmallVector<LLT, 4> SplitTys;
+  computeValueLLTs(DL, *Val->getType(), SplitTys, nullptr);
+
+  assert(SplitTys.size() == VRegs.size() &&
+         "Number of VRegs must equal number of types.");
+
+  MachineInstrBuilder Instr = MIRBuilder.buildInstrNoInsert(MOS6502::RETURN);
+
+  for (size_t i = 0; i < VRegs.size(); ++i) {
+    const LLT &Ty = SplitTys[i];
+    const Register VReg = VRegs[i];
+    assert(Ty.isByteSized() && "Must divide evenly into bytes.");
+    unsigned NumBytes = Ty.getSizeInBytes();
+
+    if (NumBytes == 1) {
+      Instr.addUse(VReg);
+      continue;
+    }
+
+    SmallVector<Register, 4> ExpandedVRegs;
+    LLT s8 = LLT::scalar(8);
+    for (unsigned j = 0; j < NumBytes; ++j) {
+      const Register VReg = MIRBuilder.getMRI()->createGenericVirtualRegister(s8);
+      Instr.addUse(VReg);
+      ExpandedVRegs.push_back(VReg);
+    }
+    MIRBuilder.buildUnmerge(ExpandedVRegs, VReg);
   }
 
+  MIRBuilder.insertInstr(Instr);
   return true;
 }
