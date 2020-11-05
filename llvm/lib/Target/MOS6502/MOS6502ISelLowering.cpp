dfb0d9adb712ce812b0d50b4ebd0ecaab88c9835
Daniel Thornburgh <mysterymath@gmail.com>
2020-11-05 10:42:49 -0800

    Add reg classes.

--- llvm/lib/Target/MOS6502/MOS6502ISelLowering.cpp
+++ llvm/lib/Target/MOS6502/MOS6502ISelLowering.cpp
@@ -1,5 +1,6 @@
 #include "MOS6502ISelLowering.h"
 
+#include "MOS6502RegisterInfo.h"
 #include "MOS6502Subtarget.h"
 
 using namespace llvm;
@@ -7,5 +8,7 @@ using namespace llvm;
 MOS6502TargetLowering::MOS6502TargetLowering(const TargetMachine &TM,
                                              const MOS6502Subtarget& STI)
   : TargetLowering(TM) {
+  addRegisterClass(MVT::i8, &MOS6502::I8RegClass);
+  addRegisterClass(MVT::i16, &MOS6502::I16RegClass);
   computeRegisterProperties(STI.getRegisterInfo());
 }
