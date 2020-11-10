230f421c26dac042f5e2b1410d1b27410c510dc1
Daniel Thornburgh <mysterymath@gmail.com>
2020-11-10 12:25:18 -0800

    Stub prolog and epilog.

--- llvm/lib/Target/MOS6502/MOS6502FrameLowering.cpp
+++ llvm/lib/Target/MOS6502/MOS6502FrameLowering.cpp
@@ -8,12 +8,12 @@ MOS6502FrameLowering::MOS6502FrameLowering()
   : TargetFrameLowering(StackGrowsDown, /*StackAlignment=*/Align(1),
                         /*LocalAreaOffset=*/0) {}
 
-void MOS6502FrameLowering::emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const {
-  assert(0 && "Not yet implemented.");
-}
-void MOS6502FrameLowering::emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const {
-  assert(0 && "Not yet implemented.");
-}
+void MOS6502FrameLowering::emitPrologue(MachineFunction &MF,
+                                        MachineBasicBlock &MBB) const {}
+
+void MOS6502FrameLowering::emitEpilogue(MachineFunction &MF,
+                                        MachineBasicBlock &MBB) const {}
+
 bool MOS6502FrameLowering::hasFP(const MachineFunction &MF) const {
   const MachineFrameInfo &MFI = MF.getFrameInfo();
   return MFI.isFrameAddressTaken() || MFI.hasVarSizedObjects();
