8fc40178d6562ceff0bb94148e4eb5fc6c82c386
Daniel Thornburgh <mysterymath@gmail.com>
2020-11-14 11:39:53 -0800

    Implement MachineInstrToMCInst lowering.

--- llvm/lib/Target/MOS6502/MOS6502MCInstLower.cpp
+++ llvm/lib/Target/MOS6502/MOS6502MCInstLower.cpp
@@ -3,8 +3,33 @@
 
 using namespace llvm;
 
+static bool LowerMOS6502MachineOperandToMCOperand(const MachineOperand &MO,
+                                                  MCOperand &MCOp,
+                                                  const AsmPrinter &AP) {
+  switch (MO.getType()) {
+  default:
+    report_fatal_error("Unknown operand type.");
+  case MachineOperand::MO_Register:
+    // Ignore all implicit register operands.
+    if (MO.isImplicit())
+      return false;
+    MCOp = MCOperand::createReg(MO.getReg());
+    break;
+  case MachineOperand::MO_Immediate:
+    MCOp = MCOperand::createImm(MO.getImm());
+    break;
+  }
+  return true;
+}
+
 void llvm::LowerMOS6502MachineInstrToMCInst(const MachineInstr *MI,
                                             MCInst &OutMI,
                                             const AsmPrinter &AP) {
-  report_fatal_error("Not yet implemented.");
+  OutMI.setOpcode(MI->getOpcode());
+
+  for (const MachineOperand &MO : MI->operands()) {
+    MCOperand MCOp;
+    if (LowerMOS6502MachineOperandToMCOperand(MO, MCOp, AP))
+      OutMI.addOperand(MCOp);
+  }
 }
