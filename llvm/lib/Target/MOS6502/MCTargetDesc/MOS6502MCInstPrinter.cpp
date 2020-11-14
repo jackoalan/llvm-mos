981a36389d1015b80077ced6dc4bc4da629ae1b7
Daniel Thornburgh <mysterymath@gmail.com>
2020-11-14 11:54:52 -0800

    Add tablegen inst printing code.

--- llvm/lib/Target/MOS6502/MCTargetDesc/MOS6502MCInstPrinter.cpp
+++ llvm/lib/Target/MOS6502/MCTargetDesc/MOS6502MCInstPrinter.cpp
@@ -1,7 +1,13 @@
 #include "MOS6502MCInstPrinter.h"
 
+#include "llvm/MC/MCInst.h"
+#include "llvm/Support/raw_ostream.h"
+
 using namespace llvm;
 
+#define DEBUG_TYPE "asm-printer"
+
+#include "MOS6502GenAsmWriter.inc"
 
 void MOS6502InstPrinter::printInst(const MCInst *MI, uint64_t Address,
                                    StringRef Annot, const MCSubtargetInfo &STI,
