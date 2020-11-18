099cdfe9191e94fc5b569ffa90842ffa7d46aa16
Daniel Thornburgh <mysterymath@gmail.com>
2020-11-18 00:29:30 -0800

    Automatically pass through post-isel opcodes through instruction selection.

--- llvm/lib/Target/MOS6502/MOS6502InstructionSelector.cpp
+++ llvm/lib/Target/MOS6502/MOS6502InstructionSelector.cpp
@@ -19,34 +19,31 @@ using namespace llvm;
 using namespace MIPatternMatch;
 
 bool MOS6502InstructionSelector::select(MachineInstr &I) {
-  switch (I.getOpcode()) {
-  case MOS6502::RTS:
+  const MachineRegisterInfo &MRI = I.getMF()->getRegInfo();
+
+  if (!I.isPreISelOpcode()) {
+    switch (I.getOpcode()) {
+    case MOS6502::COPY: {
+      Register Dst = I.getOperand(0).getReg();
+      if (!MOS6502::GPRRegClass.contains(Dst))
+        break;
+      Optional<int64_t> Cst = getConstantVRegVal(I.getOperand(1).getReg(), MRI);
+      if (!Cst)
+        break;
+      assert(*Cst < 256);
+      MachineIRBuilder Builder(I);
+      Builder.buildInstr(MOS6502::LDimm).addDef(Dst).addImm(*Cst);
+      I.removeFromParent();
+      break;
+    }
+    }
     return true;
   }
 
-  const MachineRegisterInfo& MRI = I.getMF()->getRegInfo();
-
-  switch (I.getOpcode()) {
-  case MOS6502::COPY: {
-    Register Dst = I.getOperand(0).getReg();
-    if (!MOS6502::GPRRegClass.contains(Dst)) break;
-    Optional<int64_t> Cst = getConstantVRegVal(I.getOperand(1).getReg(), MRI);
-    if (!Cst) break;
-    assert(*Cst < 256);
-    MachineIRBuilder Builder(I);
-    Builder.buildInstr(MOS6502::LDimm).addDef(Dst).addImm(*Cst);
-    I.removeFromParent();
-    break;
-  }
-  }
-
-  switch (I.getOpcode()) {
-  case MOS6502::COPY:
-    return true;
-  }
   return false;
 }
 
-void MOS6502InstructionSelector::setupGeneratedPerFunctionState(MachineFunction &MF) {
+void MOS6502InstructionSelector::setupGeneratedPerFunctionState(
+    MachineFunction &MF) {
   // Disable assertion in base class; we're not using SelectionDAG TableGen.
 }
