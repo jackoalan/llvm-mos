1288214ba45943a25b56cf48a46ff05a8afc0e98
Daniel Thornburgh <mysterymath@gmail.com>
2020-11-24 18:19:13 -0800

    Select very restricted BNE

--- llvm/lib/Target/MOS6502/MOS6502InstructionSelector.cpp
+++ llvm/lib/Target/MOS6502/MOS6502InstructionSelector.cpp
@@ -10,6 +10,7 @@
 #include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 #include "llvm/CodeGen/GlobalISel/RegisterBankInfo.h"
 #include "llvm/CodeGen/GlobalISel/Utils.h"
+#include "llvm/CodeGen/MachineBasicBlock.h"
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/TargetInstrInfo.h"
@@ -42,6 +43,8 @@ private:
   const MOS6502InstrInfo &TII;
   const MOS6502RegisterInfo &TRI;
   const MOS6502RegisterBankInfo &RBI;
+
+  bool selectCompareBranch(MachineInstr &I, MachineRegisterInfo& MRI);
 
   /// tblgen-erated 'select' implementation, used as the initial selector for
   /// the patterns that don't require complex C++.
@@ -76,10 +79,44 @@ MOS6502InstructionSelector::MOS6502InstructionSelector(
 }
 
 bool MOS6502InstructionSelector::select(MachineInstr &I) {
-  if (!I.isPreISelOpcode()) {
+  if (!I.isPreISelOpcode())
     return true;
+  if (selectImpl(I, *CoverageInfo))
+    return true;
+
+  MachineRegisterInfo &MRI = I.getParent()->getParent()->getRegInfo();
+
+  switch(I.getOpcode()) {
+  default:
+    return false;
+  case MOS6502::G_BRCOND:
+    return selectCompareBranch(I, MRI);
   }
-  return selectImpl(I, *CoverageInfo);
+}
+
+bool MOS6502InstructionSelector::selectCompareBranch(MachineInstr &I, MachineRegisterInfo &MRI) {
+  assert(I.getOpcode() == MOS6502::G_BRCOND);
+  Register CondReg = I.getOperand(0).getReg();
+  MachineBasicBlock *Tgt = I.getOperand(1).getMBB();
+  MachineInstr *CCMI = MRI.getVRegDef(CondReg);
+  if (CCMI->getOpcode() != MOS6502::G_ICMP)
+    return false;
+  auto Pred = static_cast<CmpInst::Predicate>(CCMI->getOperand(1).getPredicate());
+  if (Pred != CmpInst::ICMP_NE)
+    return false;
+
+  auto L = CCMI->getOperand(2).getReg();
+  auto R = getConstantVRegValWithLookThrough(CCMI->getOperand(3).getReg(), MRI);
+  if (!R)
+    return false;
+
+  MachineIRBuilder Builder(I);
+  MachineInstrBuilder Compare = Builder.buildInstr(MOS6502::CMPimm).addUse(L).addImm(R->Value);
+  if (!constrainSelectedInstRegOperands(*Compare, TII, TRI, RBI))
+    return false;
+  Builder.buildInstr(MOS6502::BNE).addMBB(Tgt);
+  I.eraseFromParent();
+  return true;
 }
 
 InstructionSelector *
