5601508195595bc8784ef55dbf1e6b0384da10e9
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-01 18:36:00 -0800

    Register copies with push and pull (some problems.)

--- llvm/lib/Target/MOS6502/MOS6502InstrInfo.cpp
+++ llvm/lib/Target/MOS6502/MOS6502InstrInfo.cpp
@@ -1,9 +1,9 @@
 #include "MOS6502InstrInfo.h"
-#include "MOS6502RegisterInfo.h"
 #include "MCTargetDesc/MOS6502MCTargetDesc.h"
+#include "MOS6502RegisterInfo.h"
+#include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 #include "llvm/CodeGen/GlobalISel/Utils.h"
 #include "llvm/Support/ErrorHandling.h"
-#include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 
 using namespace llvm;
 
@@ -14,23 +14,42 @@ void MOS6502InstrInfo::copyPhysReg(MachineBasicBlock &MBB,
                                    MachineBasicBlock::iterator MI,
                                    const DebugLoc &DL, MCRegister DestReg,
                                    MCRegister SrcReg, bool KillSrc) const {
-  if (!MOS6502::GPRRegClass.contains(DestReg) ||
-      !MOS6502::GPRRegClass.contains(SrcReg)) {
+  MachineIRBuilder Builder(MBB, MI);
+
+  const auto &areClasses = [&](const TargetRegisterClass &Src,
+                               const TargetRegisterClass &Dest) {
+    return Src.contains(SrcReg) && Dest.contains(DestReg);
+  };
+
+  bool preserveP = MBB.computeRegisterLiveness(
+                       MBB.getParent()->getSubtarget().getRegisterInfo(),
+                       MOS6502::P, MI) == MachineBasicBlock::LQR_Dead;
+
+  if (preserveP)
+    Builder.buildInstr(MOS6502::PHP);
+
+  if (areClasses(MOS6502::GPRRegClass, MOS6502::GPRRegClass)) {
+    if (SrcReg == MOS6502::A) {
+      assert(MOS6502::XYRegClass.contains(DestReg));
+      Builder.buildInstr(MOS6502::TA_).addUse(DestReg);
+    } else if (DestReg == MOS6502::A) {
+      assert(MOS6502::XYRegClass.contains(SrcReg));
+      Builder.buildInstr(MOS6502::T_A).addUse(SrcReg);
+    } else {
+      copyPhysReg(MBB, Builder.getInsertPt(), Builder.getDebugLoc(), MOS6502::A,
+                  SrcReg, KillSrc);
+      copyPhysReg(MBB, Builder.getInsertPt(), Builder.getDebugLoc(), DestReg,
+                  MOS6502::A, true);
+      Builder.buildInstr(MOS6502::PLA);
+    }
+  } else if (areClasses(MOS6502::GPRRegClass, MOS6502::ZPRegClass)) {
+    Builder.buildInstr(MOS6502::STzp).addDef(DestReg).addUse(SrcReg);
+  } else if (areClasses(MOS6502::ZPRegClass, MOS6502::GPRRegClass)) {
+    Builder.buildInstr(MOS6502::LDzp).addDef(DestReg).addUse(SrcReg);
+  } else {
     report_fatal_error("Unsupported physical register copy.");
   }
-  if (SrcReg == MOS6502::A) {
-    assert(MOS6502::XYRegClass.contains(DestReg));
-    MachineIRBuilder Builder(MBB, MI);
-    Builder.buildInstr(MOS6502::TA_).addUse(DestReg);
-  } else if (DestReg == MOS6502::A) {
-    assert(MOS6502::XYRegClass.contains(SrcReg));
-    MachineIRBuilder Builder(MBB, MI);
-    Builder.buildInstr(MOS6502::T_A).addUse(SrcReg);
-  } else {
-    MachineIRBuilder Builder(MBB, MI);
-    Builder.buildInstr(MOS6502::PHA);
-    copyPhysReg(MBB, Builder.getInsertPt(), Builder.getDebugLoc(), MOS6502::A, SrcReg, KillSrc);
-    copyPhysReg(MBB, Builder.getInsertPt(), Builder.getDebugLoc(), DestReg, MOS6502::A, true);
-    Builder.buildInstr(MOS6502::PLA);
-  }
+
+  if (preserveP)
+    Builder.buildInstr(MOS6502::PLP);
 }
