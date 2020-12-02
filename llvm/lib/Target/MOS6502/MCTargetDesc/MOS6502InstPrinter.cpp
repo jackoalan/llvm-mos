0ecfca359ce32f8c39d3093fed62b62312a0e8c4
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-02 12:32:15 -0800

    Print custom instructions.

--- llvm/lib/Target/MOS6502/MCTargetDesc/MOS6502InstPrinter.cpp
+++ llvm/lib/Target/MOS6502/MCTargetDesc/MOS6502InstPrinter.cpp
@@ -1,5 +1,6 @@
 #include "MOS6502InstPrinter.h"
-
+#include "MOS6502MCTargetDesc.h"
+#include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -11,17 +12,55 @@ using namespace llvm;
 
 void MOS6502InstPrinter::printInst(const MCInst *MI, uint64_t Address,
                                    StringRef Annot, const MCSubtargetInfo &STI,
-                                   raw_ostream &O) {
-  printInstruction(MI, Address, O);
+                                   raw_ostream &OS) {
+  switch (MI->getOpcode()) {
+  default:
+    printInstruction(MI, Address, OS);
+    break;
+  case MOS6502::CMPimm: {
+    unsigned Reg = MI->getOperand(0).getReg();
+    switch (Reg) {
+    default:
+      llvm_unreachable("Invalid CMPimm register.");
+    case MOS6502::A:
+      OS << "CMP\t#";
+      break;
+    case MOS6502::X:
+      OS << "CPX\t#";
+      break;
+    case MOS6502::Y:
+      OS << "CPY\t#";
+      break;
+    }
+    printOperand(MI, 1, OS);
+    break;
+  }
+  case MOS6502::LDCimm: {
+    int64_t Imm = MI->getOperand(0).getImm();
+    switch (Imm) {
+    default:
+      llvm_unreachable("Invalid LDCimm immediate.");
+    case 0:
+      OS << "CLC";
+      break;
+    case 1:
+      OS << "SEC";
+      break;
+    }
+    break;
+  }
+  }
 }
 
 void MOS6502InstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
-                                      raw_ostream &O) {
+                                      raw_ostream &OS) {
   const MCOperand &MO = MI->getOperand(OpNo);
   if (MO.isReg()) {
-    printRegName(O, MO.getReg());
+    printRegName(OS, MO.getReg());
   } else if (MO.isImm()) {
-    O << MO.getImm();
+    OS << MO.getImm();
+  } else if (MO.isExpr()) {
+    MO.getExpr()->print(OS, &MAI);
   } else {
     report_fatal_error("Unknown operand kind.");
   }
