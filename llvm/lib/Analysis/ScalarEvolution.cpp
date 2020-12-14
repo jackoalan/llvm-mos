80f79095f093624b357a1c0b2d9d33625d3a71f9
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-14 14:45:32 -0800

    Fix SCEV regression.

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -8317,8 +8317,8 @@ const SCEV *ScalarEvolution::computeExitCountExhaustively(const Loop *L,
 
     if (CondVal->getValue() == uint64_t(ExitWhen)) {
       ++NumBruteForceTripCountsComputed;
-      APInt Count(IterationNum, 32);
-      return getConstant(Count.trunc(Count.getActiveBits()));
+      APInt Count(32, IterationNum);
+      return getConstant(Count.trunc(std::max(Count.getActiveBits(), 1u)));
     }
 
     // Update all the PHI nodes for the next iteration.
