d2bef15901e25eee3e92cc5075dc562e12d26a08
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-21 15:18:04 -0800

    Try real cmake dir.

--- .build.sh
+++ .build.sh
@@ -2,5 +2,5 @@
 mkdir build
 cd build
 set -ev
-/usr/local/bin/cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
+/usr/local/cmake/bin/cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
 ninja check-all
