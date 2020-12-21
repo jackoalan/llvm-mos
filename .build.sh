b1f8129279877de175e237dda291279871b5c725
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-21 15:30:34 -0800

    Revert "Meh kill Travis."
    This reverts commit 2573b95babdf067063b5a3a9d98677f9f8a1ed17.

---
+++ .build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir build
+cd build
+set -ev
+/usr/local/cmake/bin/cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
+ninja check-all
