f085778801825cccb7f84c8da503c4f772a66644
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-21 15:16:04 -0800

    Reenable all targets for CI build.

--- .build.sh
+++ .build.sh
@@ -2,5 +2,5 @@
 mkdir build
 cd build
 set -ev
-/usr/local/bin/cmake -DLLVM_TARGETS_TO_BUILD="AArch64" -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
+/usr/local/bin/cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
 ninja check-all
