4abc6aad1f0d989ba1dd95e5f0509285b2a3aa89
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-21 17:58:18 -0800

    Replace Travis with Github actions from upstream.

--- .build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir build
-cd build
-set -ev
-/snap/cmake/current/bin/cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
-ninja check-all
