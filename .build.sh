c5e7908fd31306fc8f2ae3611f09d5f9ac3de181
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-21 16:27:29 -0800

    Try installing using snap.

--- .build.sh
+++ .build.sh
@@ -2,5 +2,5 @@
 mkdir build
 cd build
 set -ev
-cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
+/snap/cmake/current/bin/cmake -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="MOS6502" -DLLVM_USE_LINKER="lld" -DLLVM_ENABLE_PROJECTS="clang" -G "Ninja" ../llvm
 ninja check-all
