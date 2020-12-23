da1d4055c1a9b8bbcc0c6bab980fe9fdf2e08426
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-22 16:27:53 -0800

    Add test cases up to hello_world milestone.

---
+++ llvm/test/CodeGen/MOS6502/hello_world.c
@@ -0,0 +1,34 @@
+// RUN: split-file --no-leading-lines %s %t
+// RUN: clang --target=mos6502 -Os -S -o %t/got.s %t/hello_world.c
+// RUN: diff -u %t/want.s %t/got.s
+
+//--- hello_world.c
+int main(void) {
+	const char *cur = "HELLO, WORLD!\n";
+	while (*cur) {
+		char c = *cur++;
+		asm volatile ("JSR\t$FFD2" : "+a"(c));
+	}
+}
+//--- want.s
+.code
+.global	main
+main:
+	LDX	#0
+	LDA	#72
+LBB0__1:
+	;APP
+	JSR	$FFD2
+	;NO_APP
+	LDA	_2Estr+1,X
+	INX
+	CPX	#14
+	BNE	LBB0__1
+	LDA	#0
+	LDX	#0
+	RTS
+
+.rodata
+_2Estr:
+	.byt	72,69,76,76,79,44,32,87,79,82,76,68,33,10,0
+
