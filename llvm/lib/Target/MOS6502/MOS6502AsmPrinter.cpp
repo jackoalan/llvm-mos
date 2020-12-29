799634d085be903a2d0cf7a3493b511643c15f7a
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-29 10:48:20 -0800

    Emit declarations for external global symbols.

--- llvm/lib/Target/MOS6502/MOS6502AsmPrinter.cpp
+++ llvm/lib/Target/MOS6502/MOS6502AsmPrinter.cpp
@@ -1,5 +1,6 @@
 #include "MOS6502MCInstLower.h"
 #include "TargetInfo/MOS6502TargetInfo.h"
+#include "llvm/ADT/StringSet.h"
 #include "llvm/CodeGen/AsmPrinter.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/Support/TargetRegistry.h"
@@ -12,6 +13,7 @@ namespace {
 
 class MOS6502AsmPrinter : public AsmPrinter {
   MOS6502MCInstLower InstLowering;
+  SmallPtrSet<MCSymbol*, 32> ExternalSymbols;
 
 public:
   explicit MOS6502AsmPrinter(TargetMachine &TM,
@@ -19,12 +21,25 @@ public:
       : AsmPrinter(TM, std::move(Streamer)), InstLowering(OutContext, *this) {}
 
   void emitInstruction(const MachineInstr *MI) override;
+
+  void emitEndOfAsmFile(Module &) override;
 };
 
 void MOS6502AsmPrinter::emitInstruction(const MachineInstr *MI) {
+  for (const MachineOperand &MO : MI->operands())
+    if (MO.isSymbol())
+      ExternalSymbols.insert(OutContext.getOrCreateSymbol(MO.getSymbolName()));
+
   MCInst Inst;
   InstLowering.lower(MI, Inst);
   EmitToStreamer(*OutStreamer, Inst);
+}
+
+void MOS6502AsmPrinter::emitEndOfAsmFile(Module &M) {
+  // Unlike GAS, ca65 requires declaring all used symbols, even if external.
+  for (const auto &S : ExternalSymbols)
+    OutStreamer->emitSymbolAttribute(S, MCSA_Global);
+  ExternalSymbols.clear();
 }
 
 } // namespace
