88089fa56552a199a1b7c755c1dc6365eb003e10
Daniel Thornburgh <mysterymath@gmail.com>
2020-12-30 17:22:02 -0800

    Fix discovered issues with prolog/epilog insertion.

--- llvm/lib/Target/MOS6502/MOS6502FrameLowering.cpp
+++ llvm/lib/Target/MOS6502/MOS6502FrameLowering.cpp
@@ -6,6 +6,7 @@
 #include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 #include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineOperand.h"
 #include "llvm/Support/ErrorHandling.h"
 
 using namespace llvm;
@@ -23,18 +24,39 @@ void MOS6502FrameLowering::emitPrologue(MachineFunction &MF,
   MachineIRBuilder Builder(MBB, MBB.begin());
   // It doesn't matter what is pushed, just that the stack pointer is increased.
   // This is still at least as efficient as increasing S using X and/or A.
-  for (uint64_t I = 0; I < MFI.getStackSize(); ++I)
-    Builder.buildInstr(MOS6502::PHA);
+  for (uint64_t I = 0; I < MFI.getStackSize(); ++I) {
+    auto Push = Builder.buildInstr(MOS6502::PHA);
+    Push->implicit_operands().begin()->setIsUndef();
+  }
 }
 
 void MOS6502FrameLowering::emitEpilogue(MachineFunction &MF,
                                         MachineBasicBlock &MBB) const {
   const MachineFrameInfo &MFI = MF.getFrameInfo();
-  MachineIRBuilder Builder(MBB, MBB.getFirstTerminator());
+  if (!MFI.getStackSize())
+    return;
+
+  const TargetRegisterInfo &TRI = *MF.getSubtarget().getRegisterInfo();
+
+  auto MI = MBB.getFirstTerminator();
+  if (MI == MBB.end())
+    --MI;
+
+  MachineIRBuilder Builder(MBB, MI);
+  bool AMaybeLive = MBB.computeRegisterLiveness(&TRI, MOS6502::A, MI) != MachineBasicBlock::LQR_Dead;
+  assert(MBB.computeRegisterLiveness(&TRI, MOS6502::NZ, MI) != MachineBasicBlock::LQR_Live);
+
+
+  if (AMaybeLive)
+    Builder.buildInstr(MOS6502::STzpr).addDef(MOS6502::ZP_0).addUse(MOS6502::A);
+
   // It doesn't matter what is pushed, just that the stack pointer is increased.
   // This is still at least as efficient as increasing S using X and/or A.
   for (uint64_t I = 0; I < MFI.getStackSize(); ++I)
     Builder.buildInstr(MOS6502::PLA);
+
+  if (AMaybeLive)
+    Builder.buildInstr(MOS6502::LDzpr).addDef(MOS6502::A).addUse(MOS6502::ZP_0);
 }
 
 bool MOS6502FrameLowering::hasFP(const MachineFunction &MF) const {
