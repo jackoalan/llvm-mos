a644227e34b40df9bb6c479d562b4cb77e23dc9c
Daniel Thornburgh <mysterymath@gmail.com>
2021-01-07 00:38:48 -0800

    Add option for fine-grained tracking of register pressure.

--- llvm/utils/TableGen/CodeGenRegisters.h
+++ llvm/utils/TableGen/CodeGenRegisters.h
@@ -342,6 +342,9 @@ namespace llvm {
     /// Generate register pressure set for this register class and any class
     /// synthesized from it.
     bool GeneratePressureSet;
+    // Disable register pressure set pruning for this class and any class
+    // synthesized from it.
+    bool IsPressureFineGrained;
 
     // Return the Record that defined this class, or NULL if the class was
     // created by TableGen.
@@ -515,8 +518,9 @@ namespace llvm {
 
     std::string Name;
     std::vector<unsigned> Units;
-    unsigned Weight = 0; // Cache the sum of all unit weights.
-    unsigned Order = 0;  // Cache the sort key.
+    unsigned Weight = 0;          // Cache the sum of all unit weights.
+    unsigned Order = 0;          // Cache the sort key.
+    bool IsFineGrained = false;  // Disable pruning.
 
     RegUnitSet() = default;
   };
